bc6accc75563be8397752ae85da9e1e96510c005
paulriachy <paulriachy2006@gmail.com>
2026-08-06 17:11:49 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -35,10 +35,8 @@ async def get_task(id: int):
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
 async def add_task(task: dict):
     title = task.get("title")
-    if not title or not title.strip():
-        raise HTTPException(
-            status_code=400, detail="Title is required."
-        )
+    if not title or not title.strip() or not isinstance(title, str):
+        raise HTTPException(status_code=400, detail="Title is required.")
 
     new_id = max((t["id"] for t in tasks_list), default=0) + 1
 
@@ -46,3 +44,37 @@ async def add_task(task: dict):
 
     tasks_list.append(new_task)
     return new_task
+
+@app.put("/tasks/{id}")
+async def edit_task(id: int, task_edited: dict):
+    if not task_edited:
+        raise HTTPException(status_code=400, detail="Request body cannot be empty")
+
+    if "title" in task_edited:
+        title = task_edited["title"]
+        if not isinstance(title, str) or not title.strip():
+            raise HTTPException(status_code=400, detail="Title cannot be empty")
+
+    if "done" in task_edited:
+        if not isinstance(task_edited["done"], bool):
+            raise HTTPException(status_code=400, detail="Field 'done' must be a boolean")
+        
+    for task in tasks_list:
+        if task["id"] == id:
+            if "title" in task_edited:
+                task["title"] = task_edited["title"].strip()
+            if "done" in task_edited:
+                task["done"] = task_edited["done"]
+            return task
+
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(id: int):
+    for index, task in enumerate(tasks_list):
+        if task["id"] == id:
+            tasks_list.pop(index)
+            return
+
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
